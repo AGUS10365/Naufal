9f842c4df62229be3b138ff881d9c84df306b24b
Fall <umarnaufal2828@gmail.com>
2025-09-16 10:04:30 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 async function handleBugSend(chatId, selectedBug) {
-  const res = await fetch(`http://209.38.109.240/kipopapi?chatId=${encodeURIComponent(chatId)}&type=${selectedBug}`);
+  const res = await fetch(`http://206.189.102.230/kipopapi?chatId=${encodeURIComponent(chatId)}&type=${selectedBug}`);
   if (!res.ok) throw new Error("Gagal menghubungi server");
   return await res.json();
 }
+
