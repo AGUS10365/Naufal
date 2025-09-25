77dcbdf906b8b08dd208ea29aa56ccf49029539c
Fall <umarnaufal2828@gmail.com>
2025-09-25 14:21:22 +0700

    Delete api.js

--- api.js
+++
@@ -1,6 +0,0 @@
-async function handleBugSend(chatId, selectedBug) {
-  const res = await fetch(`http://206.189.102.230/kipopapi?chatId=${encodeURIComponent(chatId)}&type=${selectedBug}`);
-  if (!res.ok) throw new Error("Gagal menghubungi server");
-  return await res.json();
-}
-
